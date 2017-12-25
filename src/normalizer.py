6f33783fa11329258b10d4ae94c6529580ba5109
John Aldrin Batan <johnaldrinb@gmail.com>
2017-12-25 14:04:38 +0800

    modified training data

--- src/normalizer.py
+++ src/normalizer.py
@@ -7,6 +7,31 @@ class Normalizer:
         index = 0
 
         skills = file.readlines()
-        return skills.index(skill_description + '\n')
+
+        for skill in skills:
+            skill = skill.upper()
+            print(skill)
+
+        return skills.index(skill_description.upper())
 
 
+if __name__ == '__main__':
+    normalizer = Normalizer()
+
+    job = 'skillsWeb'
+    #filename = 'data/' + job + '.txt'
+    filename = 'skills_list.txt'
+    file = open(filename, 'r')
+
+    skills = file.readlines()
+    skills_indexes = [None]
+
+    skills_set = list(set(skills))
+
+    for skill in skills_set:
+        out = open('skills_list_uniques.txt', 'a')
+
+        #skills_indexes.append(skill)
+        out.write(skill.upper())
+        out.close()
+        #skills_indexes.append(normalizer.get_skill_index(skill))

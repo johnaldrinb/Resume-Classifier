ae3319191a96739b1e04ba80041a563a2e8249dc
John Aldrin Batan <johnaldrinb@gmail.com>
2017-12-25 18:19:33 +0800

    modified training set

--- src/normalizer.py
+++ src/normalizer.py
@@ -1,3 +1,4 @@
+
 class Normalizer:
 
     def get_skill_index(self, skill_description):
@@ -18,17 +19,38 @@ class Normalizer:
 if __name__ == '__main__':
     normalizer = Normalizer()
 
-    job = 'skillsSoftDev'
-    filename = 'data/' + job + '.txt'
+    jobs = ['skillsSoftDev', 'skillsSoftEng', 'skillsSysAnal', 'skillsWeb']
+    job_index = 3
+    job = jobs[job_index]
+
+    filename = 'data/' + job + '_index.txt'
     file = open(filename, 'r')
 
     skills = file.readlines()
     skills_indexes = [None]
 
+    cols = 100
+    col_count = 0
+
+    remainder = len(skills) % 100
+
+    if remainder > 0:
+        print(len(skills))
+        print(remainder)
+        remainder = 100 - remainder
+        for i in range(remainder):
+            skills.append('-1')
+
+
     for skill in skills:
         # skills_indexes.append(normalizer.get_skill_index(skill))
-        out = open('data/' + job + '_index.txt', 'a')
-        out.write(str(normalizer.get_skill_index(skill)))
-        out.write('\n')
-        out.close()
+        col_count += 1
+        out = open('data/training_set.csv', 'a')
+        out.write(skill.rstrip() + ', ' + str(job_index))
 
+        if col_count == 100:
+            out.write('\n')
+            col_count = 0
+
+    out.write('\n')
+    out.close()
